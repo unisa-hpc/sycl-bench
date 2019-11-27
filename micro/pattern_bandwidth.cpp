8d97499b2651bdcb8086df42a379c5ecb8ebb349
Biagio Cosenza <biacos@gmail.com>
2019-11-27 15:32:11 +0100

    Create pattern_bandwidth.cpp

---
+++ micro/pattern_bandwidth.cpp
@@ -0,0 +1,1 @@
+// TODO code pattern testing read and write to a kernel, with different size of data to be transferred, either to and from the device (GPU), possibly keeping the amount of copmutation constant
