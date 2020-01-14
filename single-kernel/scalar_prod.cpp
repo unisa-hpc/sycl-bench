0b68b3b6672e46185f2f305b18eda5e27e9da97e
Sohan Lal <sohan.lal@tu-berlin.de>
2020-01-14 12:43:11 +0100

    added scalar product benchmark

--- single-kernel/scalar_prod.cpp
+++ single-kernel/scalar_prod.cpp
@@ -5,13 +5,13 @@
 //using namespace cl::sycl;
 namespace s = cl::sycl;
 class ScalarProdkernel;
+class ScalarProdReduction;
 
 class ScalarProdBench
 {
 protected:    
     std::vector<int> input1;
     std::vector<int> input2;
-    std::vector<int> temp_output;
     std::vector<int> output;
     BenchmarkArgs args;
 
@@ -19,62 +19,104 @@ public:
   ScalarProdBench(const BenchmarkArgs &_args) : args(_args) {}
   
   void setup() {      
-    //std::cout << args.problem_size << std::endl;
-      // host memory allocation
-    // TODO Fill data with values?
-    input1.resize(args.problem_size, 1);
-    input2.resize(args.problem_size, 1);
-    temp_output.resize(args.problem_size, 0);
-    // Vector product result
-    output.resize(1, 0);
+    // host memory allocation and initialization
+    input1.resize(args.problem_size);
+    input2.resize(args.problem_size);
+    output.resize(args.problem_size, 0);
+
+    for (size_t i =0; i < args.problem_size; i++) {
+      input1[i] = i;
+      input2[i] = i;
+    }
+
   }
 
   void run() {    
     s::buffer<int, 1> input1_buf(input1.data(), s::range<1>(args.problem_size));
     s::buffer<int, 1> input2_buf(input2.data(), s::range<1>(args.problem_size));
-    s::buffer<int, 1> temp_output_buf(temp_output.data(), s::range<1>(args.problem_size));
-    s::buffer<int, 1> output_buf(output.data(), s::range<1>(1));
-
+    s::buffer<int, 1> output_buf(output.data(), s::range<1>(args.problem_size));
+    
     args.device_queue.submit(
         [&](cl::sycl::handler& cgh) {
       auto in1 = input1_buf.get_access<s::access::mode::read>(cgh);
       auto in2 = input2_buf.get_access<s::access::mode::read>(cgh);
-      // Use discard_write here, otherwise the content of the host
-      // buffer must first be copied to device
-      auto temp_out = temp_output_buf.get_access<s::access::mode::discard_write>(cgh);
-      auto out = output_buf.get_access<s::access::mode::discard_write>(cgh);
-      cl::sycl::nd_range<1> ndrange (args.problem_size, 252);
+      // Use discard_write here, otherwise the content of the hostbuffer must first be copied to device
+      auto intermediate_product = output_buf.get_access<s::access::mode::discard_write>(cgh);
+
+      cl::sycl::nd_range<1> ndrange (args.problem_size, args.local_size);
 
       cgh.parallel_for<class ScalarProdKernel>(ndrange,
-        [=](cl::sycl::nd_item<1> item_id) 
+        [=](cl::sycl::nd_item<1> item) 
         {
-          size_t gid= item_id.get_global_linear_id();
-          temp_out[gid] = in1[gid] * in2[gid];
-
-          item_id.barrier(s::access::fence_space::global_space);
-
-          if (gid == 0) {
-            for(auto i = 0; i < args.problem_size; ++i)
-              out[0] += temp_out[i];
-          }
+          size_t gid= item.get_global_linear_id();
+          intermediate_product[gid] = in1[gid] * in2[gid];
         });
     });
+
+    // std::cout << "Multiplication of vectors completed" << std::endl;
+
+    auto array_size = args.problem_size;
+    auto wgroup_size = args.local_size;
+    // Not yet tested with more than 2
+    auto elements_per_thread = 2;
+
+    while (array_size!= 1) {
+      auto n_wgroups = (array_size + wgroup_size*elements_per_thread - 1)/(wgroup_size*elements_per_thread); // two threads per work item
+
+      args.device_queue.submit(
+        [&](cl::sycl::handler& cgh) {
+
+          auto global_mem = output_buf.get_access<s::access::mode::read_write>(cgh);
+      
+          // local memory for reduction
+          auto local_mem = s::accessor <int, 1, s::access::mode::read_write, s::access::target::local> {s::range<1>(wgroup_size), cgh};
+          cl::sycl::nd_range<1> ndrange (n_wgroups*wgroup_size, wgroup_size);
+    
+          cgh.parallel_for<class ScalarProdReduction>(ndrange,
+          [=](cl::sycl::nd_item<1> item) 
+            {
+              size_t gid= item.get_global_linear_id();
+              size_t lid = item.get_local_linear_id();
+
+              // initialize local memory to 0
+              local_mem[lid] = 0; 
+
+              if ((elements_per_thread * gid) < array_size) {
+                  local_mem[lid] = global_mem[elements_per_thread*gid] + global_mem[elements_per_thread*gid + 1];
+              }
+
+              item.barrier(s::access::fence_space::local_space);
+
+              for (size_t stride = 1; stride < wgroup_size; stride *= elements_per_thread) {
+                auto local_mem_index = elements_per_thread * stride * lid;
+                if (local_mem_index < wgroup_size) {
+                    local_mem[local_mem_index] = local_mem[local_mem_index] + local_mem[local_mem_index + stride];
+                }
+
+                item.barrier(s::access::fence_space::local_space);
+              }
+
+              // Only one work-item per work group writes to global memory 
+              if (lid == 0) {
+                global_mem[item.get_group_linear_id()] = local_mem[0];
+              }
+            });
+        });
+
+      array_size = n_wgroups;
+    }
   }
 
   bool verify(VerificationSetting &ver) { 
     bool pass = true;
     int expected = 0;
-    int test = 0;
+
     for(size_t i = 0; i < args.problem_size; i++) {
         expected += input1[i] * input2[i];
-        std::cout << temp_output[i];
-        test += temp_output[i];
     }
 
-    //TODO: Cleanup after barrier fix
-    std::cout << "Scalar product CPU =" << expected << std::endl;
+    std::cout << "Scalar product on CPU =" << expected << std::endl;
     std::cout << "Scalar product on Device =" << output[0] << std::endl;
-    std::cout << "Test =" << test << std::endl;
     
     if(expected != output[0]) {
       pass = false;
