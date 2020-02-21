c00f5f6a5b7f50073ba8ed97aa11825c186caa1d
Philip Salzmann <philip.salzmann@uibk.ac.at>
2020-02-21 20:44:27 +0100

    Add verification and throughput metric for sf

--- micro/sf.cpp
+++ micro/sf.cpp
@@ -27,6 +27,11 @@ public:
     output_buf.initialize(args.device_queue, s::range<1>(args.problem_size));
   }
 
+  static ThroughputMetric getThroughputMetric(const BenchmarkArgs& args) {
+    const double OP = args.problem_size * Iterations * 3;
+    return {OP / 1024.0 / 1024.0 / 1024.0, "GOP"};
+  }
+
   void run(std::vector<cl::sycl::event>& events) {
     events.push_back(args.device_queue.submit([&](s::handler& cgh) {
       auto in = input_buf.template get_access<s::access::mode::read>(cgh);
@@ -34,17 +39,37 @@ public:
 
       cgh.parallel_for<MicroBenchSpecialFuncKernel<DataT, Iterations>>(
           s::range<1>{args.problem_size}, [=](s::id<1> gid) {
-            DataT r0, v1, v2;
-            r0 = in[gid];
-            v1 = v2 = r0;
+            DataT v0, v1, v2;
+            v0 = in[gid];
+            v1 = v2 = v0;
             for(int i = 0; i < Iterations; ++i) {
-              r0 = s::cos(v1);
+              v0 = s::cos(v1);
               v1 = s::sin(v2);
-              v2 = s::tan(r0);
+              v2 = s::tan(v0);
             }
             out[gid] = v2;
           });
     }));
+  }
+
+  bool verify(VerificationSetting& ver) {
+    DataT v0, v1, v2;
+    v0 = DataT{3.14};
+    v1 = v2 = v0;
+    for(int i = 0; i < Iterations; ++i) {
+      v0 = s::cos(v1);
+      v1 = s::sin(v2);
+      v2 = s::tan(v0);
+    }
+    const DataT expected = v2;
+    auto result = output_buf.template get_access<s::access::mode::read>();
+    for(size_t i = 0; i < args.problem_size; ++i) {
+      constexpr DataT EPSILON = 1e-5;
+      if(std::abs(result[i] - expected) > EPSILON) {
+        return false;
+      }
+    }
+    return true;
   }
 
   static std::string getBenchmarkName() {
